0152f7103f79f0b7cad6c383bfda862f5bd5ffc6
Pinta365 <hello@pinta.land>
2023-02-12 17:32:48 +0100

    ex fix

--- examples/datahistory.ts
+++ examples/datahistory.ts
@@ -5,23 +5,22 @@ const installationId = 1540524; //Can be found via getInstallations(), see examp
 const thermia = new Thermia(Deno.env.get("USER"), Deno.env.get("PASSWORD"));
 await thermia.initialize();
 
+const periodStart = new Date("2023-01-25").toJSON();
+const periodEnd = new Date("2023-01-26").toJSON();
+
 //Get the all the available registers found in the history api.
-const dataGroups = await thermia.getDataHistoryAvailableRegisters(installationId);
+const dataGroups = await thermia.getDataHistoryAvailableRegisters(installationId, periodStart, periodEnd);
 
 //...
 //Do something with the 'dataGroups'.... Im going to find the outdoor temperature and thats registerId 2000 in my case.
 //...
 
 // Get history for outdoor temperature between the 2023-01-25-2023-01-26 per hour and print the data.
-const periodStart = new Date("2023-01-25").toJSON();
-const periodEnd = new Date("2023-01-26").toJSON();
+
 const history = await thermia.getDataHistoryForRegister(
     installationId,
     2000,
     "hour",
-    undefined,
     periodStart,
     periodEnd,
 );
-
-console.table(history.data);
